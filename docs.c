ee8cdb46264aa257624001fc5909c285f23c6564
riddickzhou <240581469@qq.com>
2016-05-15 18:53:06 +0800

    感觉差不多了。

--- docs.c
+++ docs.c
@@ -19,3 +19,85 @@ ngx_stream_init_session(ngx_connection_t *c)
 }
 
 
+// handler
+void
+ngx_stream_lua_content_handler(ngx_stream_session_t *s)
+{
+    ngx_stream_lua_srv_conf_t       *lscf;
+    ngx_stream_lua_ctx_t            *ctx;
+
+    ngx_log_debug1(NGX_LOG_DEBUG_STREAM, s->connection->log, 0,
+                   "stream lua content handler fd:%d",
+                   (int) s->connection->fd);
+
+    lscf = ngx_stream_get_module_srv_conf(s, ngx_stream_lua_module);
+
+    if (lscf->content_handler == NULL) {
+        dd("no content handler found");
+        ngx_stream_lua_finalize_session(s, NGX_DECLINED);
+        return;
+    }
+
+    ctx = ngx_stream_get_module_ctx(s, ngx_stream_lua_module);
+
+    dd("ctx = %p", ctx);
+
+    if (ctx == NULL) {
+        ctx = ngx_stream_lua_create_ctx(s);
+        if (ctx == NULL) {
+            ngx_stream_lua_finalize_session(s, NGX_ERROR);
+            return;
+        }
+    }
+
+    dd("entered? %d", (int) ctx->entered_content_phase);
+
+    if (ctx->entered_content_phase) {
+        dd("calling wev handler");
+        ngx_stream_lua_finalize_session(s, ctx->resume_handler(s, ctx));
+        return;
+    }
+
+    dd("setting entered");
+
+    ctx->entered_content_phase = 1;
+
+    dd("calling content handler");
+    ngx_stream_lua_finalize_session(s, lscf->content_handler(s, ctx));
+}
+ 
+// content_handler
+ngx_int_t
+ngx_stream_lua_content_handler_file(ngx_stream_session_t *s)
+{
+    lua_State                       *L;
+    ngx_int_t                        rc;
+    u_char                          *script_path;
+    ngx_stream_lua_srv_conf_t       *lscf;
+
+    lscf = ngx_stream_get_module_srv_conf(s, ngx_stream_lua_module);
+
+    script_path = ngx_stream_lua_rebase_path(s->connection->pool,
+                                             lscf->content_src.data,
+                                             lscf->content_src.len);
+
+    if (script_path == NULL) {
+        return NGX_ERROR;
+    }
+
+    L = ngx_stream_lua_get_lua_vm(s, NULL);
+
+    /*  load Lua script file (w/ cache)        sp = 1 */
+    rc = ngx_stream_lua_cache_loadfile(s->connection->log, L, script_path,
+                                       lscf->content_src_key);
+    if (rc != NGX_OK) {
+        return NGX_ERROR;
+    }
+
+    /*  make sure we have a valid code chunk */
+    ngx_stream_lua_assert(lua_isfunction(L, -1));
+
+    return ngx_stream_lua_content_by_chunk(L, s);
+}
+
+
--- src/ngx_stream_lua_timer.c
+++ src/ngx_stream_lua_timer.c
@@ -722,61 +722,15 @@ ngx_stream_lua_mgr_timer(void *data)
     ngx_stream_lua_main_conf_t  *lmcf = timer->lmcf;
     ngx_msec_t                   next_secs = (ngx_msec_t) timer->timer_secs;
 
-    int                          nargs, co_ref;
-    u_char                      *p = NULL;
-    ngx_event_t                 *ev = NULL;
+    int                          co_ref;
     ngx_stream_session_t        *s = NULL;
     ngx_connection_t            *saved_c = NULL, *c = NULL;
     ngx_stream_lua_ctx_t        *ctx = NULL;
-    ngx_stream_lua_timer_ctx_t  *tctx = NULL;
     u_char                      *script_path = NULL;
     ngx_stream_lua_cleanup_t    *cln = NULL;
     ngx_pool_t                  *pool = NULL;
+    lua_State                   *L = NULL, *co = NULL; /* the main thread */
 
-    lua_State* L = lmcf->lua, *co = NULL; /* the main thread */
-
-    if (lmcf->watcher == NULL) {
-        /* create the watcher fake connection */
-        ngx_log_debug0(NGX_LOG_DEBUG_STREAM, ngx_cycle->log, 0,
-                       "stream lua creating fake watcher connection");
-
-        if (ngx_cycle->files) {
-            saved_c = ngx_cycle->files[0];
-        }
-
-        lmcf->watcher = ngx_get_connection(0, ngx_cycle->log);
-
-        if (ngx_cycle->files) {
-            ngx_cycle->files[0] = saved_c;
-        }
-
-        if (lmcf->watcher == NULL) {
-            ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, 0,
-                          "stream lua mgr timer alloc no memory");
-            return next_secs;
-        }
-
-        /* to work around the -1 check in ngx_worker_process_cycle: */
-        lmcf->watcher->fd = (ngx_socket_t) -2;
-
-        lmcf->watcher->idle = 1;
-        lmcf->watcher->read->handler = ngx_stream_lua_abort_pending_timers;
-        lmcf->watcher->data = lmcf;
-    }
-
-    // p = ngx_alloc(sizeof(ngx_stream_lua_timer_ctx_t), timer->log);
-    // if (p == NULL) {
-    //     goto nomem;
-    // }
-
-    // tctx = (ngx_stream_lua_timer_ctx_t *) p;
-
-    // tctx->premature = 0;
-    // tctx->co_ref = co_ref;
-    // tctx->co = co;
-    // tctx->main_conf = timer->main_conf;
-    // tctx->srv_conf = timer->srv_conf;
-    // tctx->lmcf = lmcf;
     pool = ngx_create_pool(128, ngx_cycle->log);
     if (pool == NULL) {
         goto nomem;
@@ -818,7 +772,6 @@ ngx_stream_lua_mgr_timer(void *data)
 #else
 
     c->log->file = cscf->error_log->file;
-
     if (!(c->log->log_level & NGX_LOG_DEBUG_CONNECTION)) {
         c->log->log_level = cscf->error_log->log_level;
     }
@@ -827,27 +780,6 @@ ngx_stream_lua_mgr_timer(void *data)
 
     dd("lmcf: %p", lmcf);
 
-    ctx = ngx_stream_lua_create_ctx(s);
-    if (ctx == NULL || ctx->vm_state == NULL) {
-        goto failed;
-    }
-
-    // if (ctx->vm_state == NULL {
-    //     ctx->vm_state = tctx.vm_state;
-
-    //     pcln = ngx_pool_cleanup_add(s->connection->pool, 0);
-    //     if (pcln == NULL) {
-    //         goto failed;
-    //     }
-
-    //     pcln->handler = ngx_stream_lua_cleanup_vm;
-    //     pcln->data = tctx.vm_state;
-    // }
-
-    ctx->cur_co_ctx = &ctx->entry_co_ctx;
-
-    // vm = ngx_stream_lua_get_lua_vm(s, ctx);
-
     script_path = ngx_stream_lua_rebase_path(pool,
                                              timer->src_name.data,
                                              timer->src_name.len);
@@ -855,6 +787,8 @@ ngx_stream_lua_mgr_timer(void *data)
     if (script_path == NULL) {
         goto failed;
     }
+
+    L = ngx_stream_lua_get_lua_vm(s, NULL);
 
     /*  load Lua script file (w/ cache)        sp = 1 */
     rc = ngx_stream_lua_cache_loadfile(s->connection->log, L, script_path,
@@ -866,25 +800,11 @@ ngx_stream_lua_mgr_timer(void *data)
     /*  make sure we have a valid code chunk */
     ngx_stream_lua_assert(lua_isfunction(L, -1));
 
-    ngx_log_debug2(NGX_LOG_DEBUG_STREAM, ngx_cycle->log, 0,
-                   "stream lua mgr timer expired (running: %i, max: %i)",
-                   lmcf->running_timers, lmcf->max_running_timers);
-
-    // lmcf->pending_timers--;
-
-    if (lmcf->running_timers >= lmcf->max_running_timers) {
-        ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, 0,
-                      "stream lua: %i lua_max_running_timers are not enough",
-                      lmcf->max_running_timers);
-        goto failed;
-    }
-
     ngx_log_debug1(NGX_LOG_DEBUG_STREAM, s->connection->log, 0,
                    "stream lua creating new mgr timer with delay %M", next_secs);
 
     co = lua_newthread(L);
-
-    /* L stack: time func [args] thread */
+    /* L stack: func thread */
 
 #if 0
     /* TODO */
@@ -896,7 +816,7 @@ ngx_stream_lua_mgr_timer(void *data)
 
     lua_createtable(co, 0, 1);  /* co stack: new_global_tb metatable */
     ngx_stream_lua_get_globals_table(co); /* co stack: new_global_tb metatable old_global_tbl */
-    lua_setfield(co, -2, "__index");  //metatable.__index = old_global_tbl;  co stack: global_tb metatable
+    lua_setfield(co, -2, "__index");  /* metatable.__index = old_global_tbl;  co stack: global_tb metatable */
     lua_setmetatable(co, -2); // setmetatable(new_global_tb, {__index = metatable}); co stack: new_global_tb
 
     /* co stack: new_global_tb */
@@ -906,45 +826,37 @@ ngx_stream_lua_mgr_timer(void *data)
 
     dd("stack top: %d", lua_gettop(L));
 
-    // lua_xmove(vm, L, 1);    /* move coroutine from main thread to L */
-
-    /* L stack: time func [args] thread */
-    /* vm stack: empty */
-
     lua_pushvalue(L, 1);    /* copy entry function to top of L*/
     /* L stack: func thread func */
 
-    /* L stack: time func [args] thread func */
-
     lua_xmove(L, co, 1);    /* move entry function from L to co */
     /* L stack: func thread */
-
-    /* L stack: time func [args] thread */
     /* co stack: func */
 
     ngx_stream_lua_get_globals_table(co);
     lua_setfenv(co, -2);
-
     /* co stack: func */
 
     lua_pushlightuserdata(L, &ngx_stream_lua_coroutines_key);
     lua_rawget(L, LUA_REGISTRYINDEX);
     /* L stack: func thread corountines */
 
-    /* L stack: time func [args] thread corountines */
-
     lua_pushvalue(L, -2);
     /* L stack: func thread corountines thread */
 
-    /* L stack: time func [args] thread coroutines thread */
-
     co_ref = luaL_ref(L, -2);
     /* L stack: func thread corountines */
+    
     lua_pop(L, 1);
     /* L stack: func thread */
 
     cln = ngx_stream_lua_cleanup_add(s, 0);
     if (cln == NULL) {
+        goto failed;
+    }
+
+    ctx = ngx_stream_lua_create_ctx(s);
+    if (ctx == NULL) {
         goto failed;
     }
 
@@ -966,50 +878,11 @@ ngx_stream_lua_mgr_timer(void *data)
     dd("running_timers++");
     lmcf->running_timers++;
 
-    // if (s->connection) {
-    //     tctx->listening = s->connection->listening;
-
-    // } else {
-    //     tctx->listening = NULL;
-    // }
-
-    // if (s->connection->addr_text.len) {
-    //     tctx->client_addr_text.data = ngx_palloc(tctx->pool,
-    //                                              s->connection->addr_text.len);
-    //     if (tctx->client_addr_text.data == NULL) {
-    //         goto nomem;
-    //     }
-
-    //     ngx_memcpy(tctx->client_addr_text.data, s->connection->addr_text.data,
-    //                s->connection->addr_text.len);
-    //     tctx->client_addr_text.len = s->connection->addr_text.len;
-
-    // } else {
-    //     tctx->client_addr_text.len = 0;
-    //     tctx->client_addr_text.data = NULL;
-    // }
-
-    // if (ctx && ctx->vm_state) {
-    //     tctx->vm_state = ctx->vm_state;
-    //     tctx->vm_state->count++;
-
-    // } else {
-    //     tctx->vm_state = NULL;
-    // }
-
-    // lua_pushboolean(tctx.co, tctx.premature);
-
-    // n = lua_gettop(co);
-    // if (n > 2) {
-    //     lua_insert(tctx.co, 2);
-    // }
-
 #ifdef NGX_LUA_USE_ASSERT
     ctx->cur_co_ctx->co_top = 1;
 #endif
 
     ctx->entered_content_phase = 1;
-
     rc = ngx_stream_lua_run_thread(L, s, ctx, 0);
 
     dd("timer lua run thread: %d", (int) rc);
@@ -1028,7 +901,6 @@ ngx_stream_lua_mgr_timer(void *data)
     return next_secs;
 
 failed:
-
     if (co_ref && co) {
         lua_pushlightuserdata(co, &ngx_stream_lua_coroutines_key);
         lua_rawget(co, LUA_REGISTRYINDEX);
@@ -1036,17 +908,8 @@ failed:
         lua_settop(co, 0);
     }
 
-    // if (vm_state) {
-    //     ngx_stream_lua_cleanup_vm(vm_state);
-    // }
-
     if (c) {
         ngx_stream_lua_close_fake_connection(c);
-
-    }
-
-    if (pool) {
-        ngx_destroy_pool(pool);
     }
 
     ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, 0,
@@ -1055,13 +918,12 @@ failed:
     return next_secs;
 
 nomem:
-
     if (pool) {
         ngx_destroy_pool(pool);
     }
 
     ngx_log_error(NGX_LOG_ALERT, ngx_cycle->log, 0,
-                      "stream lua: mem are not enough");
+                      "stream lua: call mgr timer nomem");
 
     return next_secs;
 }
